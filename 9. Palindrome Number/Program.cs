6ac5e99c34ff44409b65b748a101f8cc905eae9b
akoc1 <72667213+akoc1@users.noreply.github.com>
2023-10-22 20:51:16 +0300

    Add files via upload

---
+++ 9. Palindrome Number/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _9._Palindrome_Number
+{
+    internal class Program
+    {
+        private bool IsPalindrome(int x){
+            string number = x.ToString();
+            int lastIndex = number.Length - 1;
+
+            if (x < 0)
+                return false;
+
+            for (int i = 0; i < number.Length / 2; i++)
+            {
+                if (number[i] != number[lastIndex])
+                    return false;
+                else
+                    lastIndex--;
+            }
+
+            return true;
+        }
+        
+        public static void Main(string[] args){
+            Program program = new Program();
+            
+            Console.WriteLine(program.IsPalindrome(905509)); // true
+        }
+    }
+}
