bd05cb9935424dd21890ba7374c68333db9e8c26
akoc1 <72667213+akoc1@users.noreply.github.com>
2023-10-22 20:08:52 +0300

    Add files via upload

---
+++ 1. Two Sum/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace two_sum
+{
+    internal class Program
+    {
+        private int[] TwoSum(int[] nums, int target) {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    int sum = nums[i] + nums[j];
+                    if (sum == target && j != i)
+                    {
+                        Console.WriteLine("[" + i.ToString() + ", " + j.ToString() + "]");
+                        return new int[] { i, j };
+                    }
+                }
+            }
+            
+            return null;
+        }
+        
+        public static void Main(string[] args)
+        {
+            Program program = new Program();
+            int[] nums = {3, 3};
+            int target = 6;
+            program.TwoSum(nums, target);
+        }
+    }
+}
