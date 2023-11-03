e2c81a9b9418708e5e6e9b95e06c32af8e92825c
akoc1 <72667213+akoc1@users.noreply.github.com>
2023-11-03 11:51:28 +0300

    Add files via upload

---
+++ 4. Median of Two Sorted Arrays/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace Median_of_Two_Sorted_Arrays
+{
+    internal class Program
+    {
+        private double FindMedianSortedArrays(int[] nums1, int[] nums2){
+            int[] mergedArray = nums1.Concat(nums2).ToArray();
+            int index;
+            double median = 0D;
+            
+            Array.Sort(mergedArray);
+
+            if (mergedArray.Length % 2 == 0){
+                index = mergedArray.Length / 2 - 1;
+                median = (mergedArray[index] + mergedArray[index + 1]) / 2D;
+            }
+            else{
+                index = (mergedArray.Length - 1) / 2;
+                median = mergedArray[index];
+            }
+            
+            return median;
+        }
+        
+        public static void Main(string[] args){
+            Program program = new Program();
+
+            Console.WriteLine(program.FindMedianSortedArrays(new int[] { 1, 2 }, new int[] { 3, 5, 4 })); // 3
+        }
+    }
+}
