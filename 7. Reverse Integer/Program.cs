85b45dde262e73515deaa22c7c268ca46f2e579f
akoc1 <72667213+akoc1@users.noreply.github.com>
2023-11-06 22:13:45 +0300

    Add files via upload

---
+++ 7. Reverse Integer/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+
+namespace _7._Reverse_Integer
+{
+    internal class Program
+    {
+        private int Reverse(int x)
+        {
+            string strX = x.ToString();
+            int starterIdx = 0, lastIdx = strX.Length - 1;
+            StringBuilder sb = new StringBuilder(strX);
+
+            if (x < 0){
+                sb[0] = '-';
+                starterIdx++;
+            }
+
+            for (var i = starterIdx; i < strX.Length; i++){
+                sb[i] = strX[lastIdx];
+                lastIdx--;
+            }
+
+            if (int.TryParse(sb.ToString(), out int result)){
+                if (result < Int32.MaxValue && result > Int32.MinValue){
+                    return result;
+                }
+            }
+
+            return 0;
+        }
+
+        public static void Main(string[] args)
+        {
+            Program program = new Program();
+
+            Console.WriteLine(program.Reverse(-250)); // -52
+        }
+    }
+}
